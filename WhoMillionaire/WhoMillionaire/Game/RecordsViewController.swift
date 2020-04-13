1463bb09797312037f24bf2fb790ff36540426cc
Stanislav <lemeshaevs@gmail.com>
2020-04-13 20:08:14 +0300

    Добавил лого и контроллер для хранения рекордов

---
+++ WhoMillionaire/WhoMillionaire/Game/RecordsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RecordsViewController.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 13.04.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
