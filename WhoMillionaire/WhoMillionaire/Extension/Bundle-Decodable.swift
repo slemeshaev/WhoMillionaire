2bc11b8a5b22ad367b6c133349f66eaf2cc945df
Stanislav <lemeshaevs@gmail.com>
2020-04-23 22:59:40 +0300

    Добавил файл decodable

---
+++ WhoMillionaire/WhoMillionaire/Extension/Bundle-Decodable.swift
@@ -0,0 +1,31 @@
+//
+//  Bundle-Decodable.swift
+//  LSChat
+//
+//  Created by Станислав Лемешаев on 16.03.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+        
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
