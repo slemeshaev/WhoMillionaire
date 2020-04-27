1ffddeba7a01c939316e16ba0190ed9e87507f65
Stanislav <lemeshaevs@gmail.com>
2020-04-28 01:43:47 +0300

    Добавил еще один экран с выигрышем

---
+++ WhoMillionaire/WhoMillionaire/Game/ScoreViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ScoreViewController.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 27.04.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
