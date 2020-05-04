f097f88d5f6c0a5e57efcedb582e1f2cba6879dd
Stanislav <lemeshaevs@gmail.com>
2020-05-04 20:35:13 +0300

    Добавил файлы для записи результатов игры

---
+++ WhoMillionaire/WhoMillionaire/Game/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 04.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ WhoMillionaire/WhoMillionaire/Helpers/RecordsCaretaker.swift
@@ -0,0 +1,9 @@
+//
+//  RecordsCaretaker.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 04.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
