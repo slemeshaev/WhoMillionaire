9220ce62e6ca5357991d99b99eb35574657f4d06
Stanislav <lemeshaevs@gmail.com>
2020-06-08 13:10:37 +0300

    Добавил необходимые файлы

---
+++ WhoMillionaire/WhoMillionaire/Model/Difficulty.swift
@@ -0,0 +1,9 @@
+//
+//  Difficulty.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 08.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ WhoMillionaire/WhoMillionaire/StrategyGame/SequentialQuestionsStrategy.swift
@@ -0,0 +1,9 @@
+//
+//  SequentialQuestionsStrategy.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 06.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
