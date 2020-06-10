403fd96b62f415fa381a3c8f65d187a847735fa9
Stanislav <lemeshaevs@gmail.com>
2020-06-10 15:08:19 +0300

    Создал extension

---
+++ WhoMillionaire/WhoMillionaire/Extension/GameViewController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewController+Extension.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 10.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
