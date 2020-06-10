adb66a2d2ca1cd7e00eb0166ca1811d589eb2114
Stanislav <lemeshaevs@gmail.com>
2020-06-10 15:08:34 +0300

    Разгрузил gameViewController

--- WhoMillionaire/WhoMillionaire/Extension/GameViewController+Extension.swift
+++ WhoMillionaire/WhoMillionaire/Extension/GameViewController+Extension.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension GameViewController {
+    
+    // функция установки progressBara
+    func setupProgressBar() {
+        progressBar.progress = 0.0
+        progressBar.layer.cornerRadius = 5
+        progressBar.clipsToBounds = true
+        progressBar.layer.sublayers![1].cornerRadius = 5
+        progressBar.subviews[1].clipsToBounds = true
+    }
+    
+    // функция отображения правильного ответа
+    func alertControllerTrueAnswer() {
+        let alert = UIAlertController(title: "Это правильный ответ!", message: "Играем дальше", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Следующий вопрос", style: .default, handler: { action in
+            self.nextQuestion()
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // функция отображения неправильного ответа
+    func alertControllerFalseAnswer() {
+        let alert = UIAlertController(title: "Это неверный ответ!", message: "Не отчаивайтесь", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Посмотреть результат", style: .default) { (action) in
+            self.performSegue(withIdentifier: "toScore", sender: nil)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // функция для поздравления миллионера
+    func alertControllerСongratulation() {
+        let yourPrice = currentQuestion.isAnswerLastQuestion ? QuestionSession.issuePrice[currentQuestion.numberQuestion-1] : QuestionSession.issuePrice[currentQuestion.numberQuestion-2]
+        let alert = UIAlertController(title: "Поздравляем!", message: "Вы выиграли \(yourPrice) рублей!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Посмотреть результат", style: .default) { (action) in
+            self.performSegue(withIdentifier: "toScore", sender: nil)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
