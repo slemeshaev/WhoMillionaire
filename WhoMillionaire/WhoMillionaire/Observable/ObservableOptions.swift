bf04c9cd3150f36c0ccf0e8b6f34f13117bf2251
Stanislav <lemeshaevs@gmail.com>
2020-06-13 21:33:15 +0300

    Добавил паттерн Observer

---
+++ WhoMillionaire/WhoMillionaire/Observable/ObservableOptions.swift
@@ -0,0 +1,9 @@
+//
+//  ObservableOptions.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 13.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
