c0310c524838b90c195009fafacf8547bf95d47f
Stanislav <lemeshaevs@gmail.com>
2020-06-14 20:41:09 +0300

    Файлы для нового viewcontroller

---
+++ WhoMillionaire/WhoMillionaire/Game/AddNewQuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewQuestionViewController.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 14.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import UIKit
+
+class AddNewQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WhoMillionaire/WhoMillionaire/Model/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  WhoMillionaire
+//
+//  Created by Станислав Лемешаев on 14.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
